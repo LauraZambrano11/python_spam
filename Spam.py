b9eb31e51d002d300412c9775161bcbe09cc94cc
Laura Zambrano <64813552+LauraZambrano11@users.noreply.github.com>
2020-12-15 23:30:48 -0500

    Update Spam.py

--- Spam.py
+++ Spam.py
@@ -1,11 +1,11 @@
 import pyautogui
 import time
 
-mensaje = "Ya quiero ir a dormir"
-n= 20
+mensaje = "Este mensaje se puede modificar" #Mensaje a imprimir
+n= 20 #Número de repeticiones
 
-time.sleep(5)
+time.sleep(5) #Tiempo de espera antes de iniciar la ejecución
 
 for i in range(n):
-    pyautogui.typewrite(mensaje + "\n")
-    #time.sleep(1)
+    pyautogui.typewrite(mensaje + "\n") #Impresión
+    #time.sleep(1)  #tiempo entre envío de mensajes, si se quita no hay espera y se envía seguido
